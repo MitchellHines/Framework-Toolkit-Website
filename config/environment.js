bc802eb657c583e4d3bb3ce44f777fd7a7f4f58e
MitchellHines <mitchell.hines@capgemini-gs.com>
2017-12-01 12:47:50 -0500

    Added github pages

--- config/environment.js
+++ config/environment.js
@@ -46,6 +46,8 @@ module.exports = function(environment) {
   if (environment === 'production') {
     ENV.locationType = 'hash';
     ENV.rootURL = '/framework-toolkit/';
+    ENV.locationType = 'hash';
+    ENV.rootURL = '/framework-toolkit/';
     // here you can enable a production-specific feature
   }
 
